5b033d6e839ccbbaf3685b8c9a1df73c81e43a75
eajung <eriikjung@gmail.com>
2019-04-29 20:18:48 -0700

    Requires sudo to edit files

--- setup.sh
+++ setup.sh
@@ -2,7 +2,13 @@
 
 # W1-GPIO - One-wire Interface
 # This requies a 4.7kohm pull-up resistor connected between the GPIO pin and 3.3V supply (e.g., pin 1 and 17) 
-echo "dtoverlay=w1-gpio" >> /boot/config.txt
+sudo echo "dtoverlay=w1-gpio" >> /boot/config.txt
 
 sudo reboot
 
+sudo modprobe w1-gpio
+sudo modprobe w1-therm
+cd /sys/bus/w1/devices
+ls
+cd 28-xxxx (change this to match what serial number pops up)
+cat w1_slave
