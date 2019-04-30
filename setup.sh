dffc4393a25f2ceac809a6de64c652c6a9a15c2c
eajung <eriikjung@gmail.com>
2019-04-29 20:24:40 -0700

    Testing crontab

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# crontab -e
+# */1* * * * sudo 'cd /home/pi/Documents/Spot_Cam/ && git pull'
 
 # W1-GPIO - One-wire Interface
 # This requies a 4.7kohm pull-up resistor connected between the GPIO pin and 3.3V supply (e.g., pin 1 and 17) 
 sudo echo "dtoverlay=w1-gpio" >> /boot/config.txt
 
-sudo reboot
+#sudo reboot
 
 sudo modprobe w1-gpio
 sudo modprobe w1-therm
